b3c115f7c3a2b923a4238e02ec90b75476ae03d6
Oran C <oranbusiness@gmail.com>
2021-12-01 14:11:42 -0700

    [add] wip

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import './style.css'
-
+import nearley from "nearley" 
+ 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+const textInput = document.getElementById("text-input")
+const output = document.getElementById("output")
+document.getElementById("parse").addEventListener("click",()=>{
+  
+  console.log("worked",textInput.value)
+
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+parser.feed(textInput.value)
+let results = parser.results[0]
+console.log(results);
+output.innerText = JSON.stringify(results)
+
+})
